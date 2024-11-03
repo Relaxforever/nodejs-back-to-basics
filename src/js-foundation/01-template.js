f8a5fc75d77e3ea0925daa82d51d48a895c142af
Relaxforever <51685896+Alafresh@users.noreply.github.com>
2024-11-03 14:31:13 -0500

    First basic stuff reintegrated

---
+++ src/js-foundation/01-template.js
@@ -0,0 +1,12 @@
+const emailTemplate = `
+<div>
+<h1>Hi, {{name}}</h1>
+<p>Thank you for signing up for our newsletter. We are excited to have you on board.</p>
+<p>Here are some of the things you can expect from us:</p>
+</div>
+
+`;
+
+module.exports = {
+    emailTemplate
+}
