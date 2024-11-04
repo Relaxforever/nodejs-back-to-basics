b9960bc9025c69d7406b40917e74ce5afb08535f
Relaxforever <ffcaicedoc@gmail.com>
2024-11-04 14:45:29 -0500

    Updated: Advanced Basics from JS/NodeJs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-const {emailTemplate} = require("./js-foundation/01-template")
-require("./js-foundation/02-destructuring")
+//const {emailTemplate} = require("./js-foundation/01-template")
+//require("./js-foundation/02-destructuring")
 //console.log(emailTemplate)
+//const { getUserById} = require("./js-foundation/03-callbacks")
+const { getUserById} = require("./js-foundation/04-arrow")
+
+
+getUserById(2,(error, user) => {
+    if (error) {
+        throw new Error(error);
+    }
+    console.log(user)
+})
--- src/js-foundation/02-destructuring.js
+++ src/js-foundation/02-destructuring.js
@@ -1,1 +1,8 @@
-console.log( process );
+//console.log( process.env);
+
+const {USERDOMAIN, WebStorm} = process.env;
+//console.table({USERDOMAIN, WebStorm});
+
+const characteres = ['Mario', 'Luigi', 'Toad', 'Bowser'];
+const [, , toad] = characteres;
+//console.log(toad);
